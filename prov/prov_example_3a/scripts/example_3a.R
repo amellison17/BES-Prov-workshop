1e4dff05dfa465b6aac04fc1d9b1572e8236ee21
Aaron Ellison <aellison@fas.harvard.edu>
2019-12-08 11:40:14 -0500

    Sunday late morning

--- prov/prov_example_3a/scripts/example_3a.R
+++ prov/prov_example_3a/scripts/example_3a.R
@@ -1,10 +1,58 @@
-#example 3a - Species occurrence data
-#data for Polyergus lucidus (a slave-making ant)
-#Working example obtaining data from GBIF
+# redraw of nutrient profiles with food enrichment
+# data from Wakefield et al. 2005
+# 25 June 2019
+# NJG
 
-Polyergus <- occ(query="Polyergus lucidus")
+# Load Libraries ----------------------------------------------------------
+library(tidyverse)
 
-P.map <- map_plot(Polyergus)
+# Source Files ------------------------------------------------------------
+# NA
+
+# Read Data ---------------------------------------------------------------
+#metadata is at https://dx.doi.org/10.6073/pasta/6591267269a71eadc859e2497e6643cf
+
+df <- read.csv(url("https://harvardforest.fas.harvard.edu/data/p32/hf328/hf328-01-sapu-prey-additions-2002.csv"))
+
+#df <- read.csv(url("https://portal.lternet.edu/nis/dataviewer?packageid=knb-lter-hfr.328.1&entityid=d21138b5d0639a3ff08ed8d67b1bf282"))
+
+# -------------------------------------------------------------------------
+theme_set(theme_bw())
+
+# Read in data and subset -------------------------------------------------
+df <- df[,c(1:3,18:22)]
+df <- rename(df,Ca.ppm=ca.ppm)
+df <- rename(df,Percent.N=pct.n)
+df <- rename(df, K.ppm=k.ppm)
+df <- rename(df, Mg.ppm=mg.ppm)
+df <- rename(df, P.ppm=p.ppm)
+
+head(df)
+# Convert to long form ----------------------------------------------------
+df_long <- gather(data=df,
+                  key="Nutrient",
+                  value="Concentration",
+                  4:8)
 
 
+# Create ggplot facet figure ----------------------------------------------
+Fig_Stoich <- ggplot(data=df_long,
+                     aes(x=as.factor(fliesperwk),
+                         y=Concentration)) +
+                       geom_boxplot() +
+                       facet_grid(Nutrient~., scales="free") +
+                       xlab("Flies added per week")
 
+plot(Fig_Stoich)
+cairo_pdf("../graphics/Fig_Stoich.pdf", height=7, width=5)
+plot(Fig_Stoich)
+dev.off()
+
+
+#A simple ANOVA on 
+
+
+p.model1 <- aov(Concentration~fliesperwk, data=df_long[df_long$Nutrient=="P.ppm",])
+summary(p.model1)
+par(mfrow=c(2,2))
+plot(p.model1)
--- src/example_3a.R
+++ src/example_3a.R
@@ -1,23 +1,56 @@
-#example 3a - Species occurrence data
-#data for Polyergus lucidus (a slave-making ant)
-#Working example obtaining data from GBIF
+# redraw of nutrient profiles with food enrichment
+# data from Wakefield et al. 2005
+# 25 June 2019
+# NJG
 
-library(spocc)
+# Load Libraries ----------------------------------------------------------
+library(tidyverse)
 
-Polyergus <- occ(query="Polyergus lucidus", from="gbif",has_coords = TRUE)
+# Source Files ------------------------------------------------------------
+# NA
 
-#Polyergus.df <- occ2df(Polyergus)
+# Read Data ---------------------------------------------------------------
+#metadata is at https://dx.doi.org/10.6073/pasta/6591267269a71eadc859e2497e6643cf
 
-#P.map <- map_plot(Polyergus)
+df <- read.csv(url("https://harvardforest.fas.harvard.edu/data/p32/hf328/hf328-01-sapu-prey-additions-2002.csv"))
 
-Polyergus.df = data.frame(Polyergus$gbif$data$Polyergus_lucidus)
+# -------------------------------------------------------------------------
+theme_set(theme_bw())
 
-worldmap <- map_data('world')
+# Read in data and subset -------------------------------------------------
+df <- df[,c(1:3,18:22)]
+df <- rename(df,Ca.ppm=ca.ppm)
+df <- rename(df,Percent.N=pct.n)
+df <- rename(df, K.ppm=k.ppm)
+df <- rename(df, Mg.ppm=mg.ppm)
+df <- rename(df, P.ppm=p.ppm)
 
-Polyergus.map <- 
-ggplot() +
-  geom_polygon(data=worldmap, fill="white", color="black", aes(x=long, y=lat, group=group)) +
-  geom_point(data=Polyergus.df, color='red',aes(x=longitude,y=latitude)) +
-  coord_quickmap()
+head(df)
+# Convert to long form ----------------------------------------------------
+df_long <- gather(data=df,
+                  key="Nutrient",
+                  value="Concentration",
+                  4:8)
 
-ggsave("../graphics/Polyergus.png")
+
+# Create ggplot facet figure ----------------------------------------------
+Fig_Stoich <- ggplot(data=df_long,
+                     aes(x=as.factor(fliesperwk),
+                         y=Concentration)) +
+                       geom_boxplot() +
+                       facet_grid(Nutrient~., scales="free") +
+                       xlab("Flies added per week")
+
+plot(Fig_Stoich)
+cairo_pdf("../graphics/Fig_Stoich.pdf", height=7, width=5)
+plot(Fig_Stoich)
+dev.off()
+
+
+#A simple ANOVA on 
+
+
+p.model1 <- aov(Concentration~fliesperwk, data=df_long[df_long$Nutrient=="P.ppm",])
+summary(p.model1)
+par(mfrow=c(2,2))
+plot(p.model1)
